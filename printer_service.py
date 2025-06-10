5aeca40a81d36c4e17659fdb6f06e910c24f8707
mablin7 <magyarb0201@gmail.com>
2025-06-10 16:33:35 +0200

    fix?

--- printer_service.py
+++ printer_service.py
@@ -82,7 +82,7 @@ class LabelPrinterService:
 
             # Print the label
             be = self.backend_class(self.printer_address)
-            time.sleep(0.01)  # this is needed for some reason??
+            time.sleep(0.5)  # this is needed for some reason??
             be.write(qlr.data)
             be.dispose()
 
